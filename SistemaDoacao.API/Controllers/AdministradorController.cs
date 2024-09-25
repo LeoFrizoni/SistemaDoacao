47fc63786b9882b1328a0ba3d5f530540c303341
Marco Ryan Marassi Marques <marcoryan2014@gmail.com>
2024-09-25 19:08:03 -0300

    add controllers

---
+++ SistemaDoacao.API/Controllers/AdministradorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaDoacao.API.Controllers
+{
+    public class AdministradorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
