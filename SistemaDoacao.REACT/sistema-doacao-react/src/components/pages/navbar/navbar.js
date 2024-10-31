3f1330562e243984339767af4a7d27d56943806c
Marco Ryan Marassi Marques <marcoryan2014@gmail.com>
2024-10-31 17:37:35 -0300

    teste navbar

--- SistemaDoacao.REACT/sistema-doacao-react/src/components/pages/navbar/navbar.js
+++ SistemaDoacao.REACT/sistema-doacao-react/src/components/pages/navbar/navbar.js
@@ -1,3 +1,33 @@
+// import React from "react";
+// import './navbar.css'
+// import logo from './imagens/logo.jpeg'
+// import { Link } from "react-router-dom";
+
+// export const Navbar = () => {
+//     return (
+//         <header>
+          
+//           <div className="logo">
+// 			      <img src={logo} alt="Logo" />
+//           </div>
+
+//           <div className="title">
+//               <p>Kindness Compass</p>
+//           </div>
+
+//           <ul className="menu">
+//             <Link to="/home">Home</Link>
+//             <Link to="/sobre nos">Sobre Nós</Link>
+//           </ul>
+
+//           <Link to="/login">
+//             <button type="button" class="btn btn-primary btn-sm" to="/login">Login</button>
+//           </Link>
+        
+//       </header>
+//     );
+// };
+
 import React from "react";
 import './navbar.css'
 import logo from './imagens/logo.jpeg'
@@ -6,24 +36,30 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
     return (
         <header>
-          
-          <div className="logo">
-			      <img src={logo} alt="Logo" />
-          </div>
+            <div className="logo-title">
+                <div className="logo">
+                    <img src={logo} alt="Logo" />
+                </div>
+                <div className="title">
+                    <p>Kindness Compass</p>
+                </div>
+            </div>
 
-          <div className="title">
-              <p>Kindness Compass</p>
-          </div>
+            <ul className="menu">
+                <li>
+                    <Link to="/home">Home</Link>
+                </li>
+                <li>
+                    <Link to="/sobre nos">Sobre Nós</Link>
+                </li>
+            </ul>
 
-          <ul className="menu">
-            <Link to="/home">Home</Link>
-            <Link to="/sobre nos">Sobre Nós</Link>
-          </ul>
+            <div className="login">
+                <Link to="/login">
+                    <button type="button" className="btn btn-primary btn-sm">Login</button>
+                </Link>
+            </div>
+        </header>
 
-          <Link to="/login">
-            <button type="button" class="btn btn-primary btn-sm" to="/login">Login</button>
-          </Link>
-        
-      </header>
     );
 };
